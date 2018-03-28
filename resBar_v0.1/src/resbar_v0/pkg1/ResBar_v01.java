923513e7fc74163fcda321dbc130d37248f02b25
marv007 <marvintorres2013@gmail.com>
2018-03-27 21:55:44 -0600

    gg izi

---
+++ resBar_v0.1/src/resbar_v0/pkg1/ResBar_v01.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package resbar_v0.pkg1;
+
+import formularios.frmInicio;
+
+/**
+ *
+ * @author user
+ */
+public class ResBar_v01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frmInicio v = new frmInicio();
+        v.setVisible(true);
+    }
+
+}
