23e8ccce21a1f6cbd37c140843c03f483bd5466b
root <root@redMonge.artist.com>
2018-03-27 22:30:20 -0600

    ejemplo Jairo

---
+++ Restaurante/src/restaurante/Restaurante.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package restaurante;
+
+import frames.ordenes;
+import java.lang.reflect.Method;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+/**
+ *
+ * @author jairoartist
+ */
+public class Restaurante {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         ordenes p=new ordenes();
+  
+         p.setVisible(true);
+ 
+ }
+    }
+    
+
