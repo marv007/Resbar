c75bdb6cb042f97fe1c63a249295c44d9b1a8a70
Marvin <Marvin@MARVIN_Dell_W10>
2018-03-30 20:22:59 -0600

    afedo camate pofavo

---
+++ src/resbar_v0/pkg1/ResBar_v01.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package resbar_v0.pkg1;
+
+import formularios.frmDashboard;
+
+
+
+/**
+ *
+ * @author user
+ */
+public class ResBar_v01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frmDashboard v = new frmDashboard();
+        v.setVisible(true);
+        v.setLocationRelativeTo(null);
+    }
+
+}
